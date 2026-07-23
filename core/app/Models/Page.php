0d90f3d6100970ed0be14880b7a7ea8b7cd77d31
pranjal030404 <pranjaltiwari69058@gmail..com>
2026-07-23 14:53:53 +0530

    updated

--- core/app/Models/Page.php
+++ core/app/Models/Page.php
@@ -6,7 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Page extends Model
 {
+    protected $fillable = [
+        'tempname',
+        'name',
+        'title',
+        'slug',
+        'is_default',
+        'secs',
+        'seo_content',
+    ];
+
     protected $casts = [
-        'seo_content'=>'object'
+        'seo_content' => 'object',
+        'secs' => 'object',
     ];
 }
--- core/database/seeders/PageSeeder.php
+++ core/database/seeders/PageSeeder.php
@@ -9,15 +9,21 @@ class PageSeeder extends Seeder
 {
     public function run(): void
     {
+        $templateName = activeTemplateName();
+
         $pages = [
-            ['title' => 'About Us',  'slug' => 'about-us'],
-            ['title' => 'Contact Us', 'slug' => 'contact-us'],
-            ['title' => 'Terms & Conditions', 'slug' => 'terms-conditions'],
-            ['title' => 'Privacy Policy', 'slug' => 'privacy-policy'],
-            ['title' => 'FAQ', 'slug' => 'faq'],
+            ['title' => 'Home', 'name' => 'Home', 'slug' => '/', 'is_default' => 1],
+            ['title' => 'About Us', 'name' => 'About Us', 'slug' => 'about-us'],
+            ['title' => 'Contact Us', 'name' => 'Contact Us', 'slug' => 'contact'],
+            ['title' => 'Terms & Conditions', 'name' => 'Terms & Conditions', 'slug' => 'terms-conditions'],
+            ['title' => 'Privacy Policy', 'name' => 'Privacy Policy', 'slug' => 'privacy-policy'],
+            ['title' => 'FAQ', 'name' => 'FAQ', 'slug' => 'faq'],
+            ['title' => 'Packages', 'name' => 'Packages', 'slug' => 'packages'],
+            ['title' => 'Stories', 'name' => 'Stories', 'slug' => 'stories'],
         ];
 
         foreach ($pages as $page) {
+            $page['tempname'] = 'templates.' . $templateName . '.';
             Page::firstOrCreate(['slug' => $page['slug']], $page);
         }
     }
