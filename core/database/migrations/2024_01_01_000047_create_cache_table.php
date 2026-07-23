4b0654c5fe5128e6b25e4361b850dd28f64b9d22
pranjal030404 <pranjaltiwari69058@gmail..com>
2026-07-23 14:46:23 +0530

    updated

--- core/database/migrations/2024_01_01_000047_create_cache_table.php
+++ core/database/migrations/2024_01_01_000047_create_cache_table.php
@@ -7,17 +7,21 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('cache', function ($table) {
-            $table->string('key')->primary();
-            $table->mediumText('value');
-            $table->integer('expiration');
-        });
+        if (!Schema::hasTable('cache')) {
+            Schema::create('cache', function ($table) {
+                $table->string('key')->primary();
+                $table->mediumText('value');
+                $table->integer('expiration');
+            });
+        }
 
-        Schema::create('cache_locks', function ($table) {
-            $table->string('key')->primary();
-            $table->string('owner');
-            $table->integer('expiration');
-        });
+        if (!Schema::hasTable('cache_locks')) {
+            Schema::create('cache_locks', function ($table) {
+                $table->string('key')->primary();
+                $table->string('owner');
+                $table->integer('expiration');
+            });
+        }
     }
 
     public function down(): void
